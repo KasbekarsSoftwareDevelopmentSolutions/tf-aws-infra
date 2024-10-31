04844e2264402b35e98f8db635eb5f7ac41af2ec
Aaditya Kasbekar <kasbekar.a@northeastern.edu>
2024-10-31 03:00:29 -0400

    S3 IAC completed.

---
+++ orgInfra/modules/s3_bucket/main.tf
@@ -0,0 +1,26 @@
+# orgInfra/modules/s3_bucket/main.tf
+resource "random_id" "s3_bucket_uuid" {
+  byte_length = 8
+}
+
+resource "aws_s3_bucket" "s3_bucket" {
+  bucket        = "${var.bucket_prefix}-${random_id.s3_bucket_uuid.hex}"
+  force_destroy = var.enable_force_destroy
+}
+
+resource "aws_s3_bucket_server_side_encryption_configuration" "encrypt" {
+  bucket = aws_s3_bucket.s3_bucket.id
+  rule {
+    apply_server_side_encryption_by_default {
+      sse_algorithm = "AES256"
+    }
+  }
+}
+
+resource "aws_s3_bucket_public_access_block" "my_private_bucket" {
+  bucket                  = aws_s3_bucket.s3_bucket.id
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+}
---
+++ orgInfra/modules/s3_bucket_lifecyclepolicy/variables.tf
@@ -0,0 +1,10 @@
+# orgInfra/modules/s3_bucket_lifecyclepolicy/variables.tf
+variable "bucket_id" {
+  description = "ID of the S3 bucket to attach the lifecycle policy to"
+  type        = string
+}
+
+variable "transition_days" {
+  description = "Number of days before transitioning to STANDARD_IA storage class"
+  type        = number
+}
