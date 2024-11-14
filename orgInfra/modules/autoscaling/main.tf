1f67af4ed6cbe32a5c290ab68baee0e518b48a2b
Aaditya Kasbekar <kasbekar.a@northeastern.edu>
2024-11-14 12:04:24 -0500

    Scalling Properties Chnaged.

--- orgInfra/modules/autoscaling/main.tf
+++ orgInfra/modules/autoscaling/main.tf
@@ -26,30 +26,91 @@ resource "aws_autoscaling_group" "app_asg" {
   }
 }
 
+# resource "aws_autoscaling_policy" "scale_up" {
+#   name                   = "scale-up-policy"
+#   policy_type            = "TargetTrackingScaling"
+#   autoscaling_group_name = aws_autoscaling_group.app_asg.name
+
+#   target_tracking_configuration {
+#     predefined_metric_specification {
+#       predefined_metric_type = "ASGAverageCPUUtilization"
+#     }
+#     target_value = var.upscale_cpu_utilization_percent
+#   }
+# }
+
+
+# resource "aws_autoscaling_policy" "scale_down" {
+#   name                   = "scale-down-policy"
+#   policy_type            = "TargetTrackingScaling"
+#   autoscaling_group_name = aws_autoscaling_group.app_asg.name
+
+#   target_tracking_configuration {
+#     predefined_metric_specification {
+#       predefined_metric_type = "ASGAverageCPUUtilization"
+#     }
+#     target_value = var.downscale_cpu_utilization_percent
+#   }
+# }
+
 resource "aws_autoscaling_policy" "scale_up" {
   name                   = "scale-up-policy"
-  policy_type            = "TargetTrackingScaling"
+  policy_type            = "StepScaling"
   autoscaling_group_name = aws_autoscaling_group.app_asg.name
+  adjustment_type        = "ChangeInCapacity"
 
-  target_tracking_configuration {
-    predefined_metric_specification {
-      predefined_metric_type = "ASGAverageCPUUtilization"
-    }
-    target_value = var.upscale_cpu_utilization_percent
+  step_adjustment {
+    metric_interval_lower_bound = var.upscale_cpu_utilization_percent
+    scaling_adjustment          = 1
   }
 }
-
 
 resource "aws_autoscaling_policy" "scale_down" {
   name                   = "scale-down-policy"
-  policy_type            = "TargetTrackingScaling"
+  policy_type            = "StepScaling"
   autoscaling_group_name = aws_autoscaling_group.app_asg.name
+  adjustment_type        = "ChangeInCapacity"
 
-  target_tracking_configuration {
-    predefined_metric_specification {
-      predefined_metric_type = "ASGAverageCPUUtilization"
-    }
-    target_value = var.downscale_cpu_utilization_percent
+  step_adjustment {
+    metric_interval_upper_bound = var.downscale_cpu_utilization_percent
+    scaling_adjustment          = -1
+  }
+
+  step_adjustment {
+    metric_interval_lower_bound = var.downscale_cpu_utilization_percent
+    metric_interval_upper_bound = null
+    scaling_adjustment          = -1
   }
 }
 
+resource "aws_cloudwatch_metric_alarm" "high_cpu" {
+  alarm_name          = "high-cpu-utilization"
+  comparison_operator = "GreaterThanThreshold"
+  evaluation_periods  = 2
+  period              = 30
+  statistic           = "Average"
+  metric_name         = "CPUUtilization"
+  namespace           = "AWS/EC2"
+  threshold           = var.upscale_cpu_utilization_percent
+  alarm_actions       = [aws_autoscaling_policy.scale_up.arn]
+
+  dimensions = {
+    AutoScalingGroupName = aws_autoscaling_group.app_asg.name
+  }
+}
+
+resource "aws_cloudwatch_metric_alarm" "low_cpu" {
+  alarm_name          = "low-cpu-utilization"
+  comparison_operator = "LessThanThreshold"
+  evaluation_periods  = 2
+  period              = 30
+  statistic           = "Average"
+  metric_name         = "CPUUtilization"
+  namespace           = "AWS/EC2"
+  threshold           = var.downscale_cpu_utilization_percent
+  alarm_actions       = [aws_autoscaling_policy.scale_down.arn]
+
+  dimensions = {
+    AutoScalingGroupName = aws_autoscaling_group.app_asg.name
+  }
+}
